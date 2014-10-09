501809032eb420f5b336d437228989ecf5aa0b11
Kobie Maitland <kobie.maitland@gmail.com>
2014-10-09 11:22:22 -0400

    Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 // Generated on 2014-08-22 using generator-angular 0.4.0
 'use strict';
-var LIVERELOAD_PORT = 35729;
+var LIVERELOAD_PORT = 35730;
 var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
